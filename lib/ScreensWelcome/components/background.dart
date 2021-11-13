f8fde8f88b331b5ff13afd3a5bd60b96db093f69
kylemoffett <kylemoffett01@gmail.com>
2021-11-13 15:33:24 +0000

    Added all files from the first time

---
+++ lib/ScreensWelcome/components/background.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key? key,
+    required this.child,
+
+  }) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size; // This size provides total height and width of our screen
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 0,
+            right:0,
+            child:Image.asset(
+              "assets/images/homeIcon.jpg",
+              width: size.width,
+            ),
+          ),
+          child,
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:moodful/ScreensWelcome/welcome_screen.dart';
+import 'package:moodful/constants.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:  false,
+      title: 'Moodful',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+    ),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
