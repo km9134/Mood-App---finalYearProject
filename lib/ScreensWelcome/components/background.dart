5ce6ac5f18850820c9af3e13ba9aab99d0bd9e45
kylemoffett <kylemoffett01@gmail.com>
2021-11-13 16:21:29 +0000

    added logo

--- lib/ScreensWelcome/components/background.dart
+++ lib/ScreensWelcome/components/background.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 
 class Background extends StatelessWidget {
   final Widget child;
+
   const Background({
     Key? key,
     required this.child,
-
   }) : super(key: key);
-
-
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size; // This size provides total height and width of our screen
+    Size size = MediaQuery.of(context)
+        .size; // This size provides total height and width of our screen
     return Container(
       height: size.height,
       width: double.infinity,
@@ -20,13 +19,19 @@ class Background extends StatelessWidget {
         children: <Widget>[
           Positioned(
             bottom: 0,
-            right:0,
-            child:Image.asset(
+            right: 0,
+            child: Image.asset(
               "assets/images/homeIcon.jpg",
               width: size.width,
             ),
           ),
-          child,
+          Positioned(
+            child: Image.asset(
+              "assets/images/logo.jpg",
+              width: size.width,
+              //alignment: Alignment.topCenter,
+            ),
+          ),
         ],
       ),
     );
