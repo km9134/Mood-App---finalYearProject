61b4c435a4c5d3224d08de0ce6e96ea6aa46f468
kylemoffett <kylemoffett01@gmail.com>
2021-11-16 21:16:59 +0000

    corrected background.dart added buttons

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -4,40 +4,56 @@ import 'package:moodful/Screens/Welcome/components/background.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery
-        .of(context)
-        .size;
+    Size size = MediaQuery.of(context).size;
     // This size provide us total height and width of our screen
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Stack(
+        //mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(
-            "WELCOME",
-            style: TextStyle(fontWeight: FontWeight.bold),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              child: Text(
+                "WELCOME",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
           ),
-          ElevatedButton(
-            style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: BorderSide(color: Colors.teal, width: 2.0)))),
-            child: Text('Sign Up'),
-            onPressed: () {},
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 80, top: 80),
+              padding: const EdgeInsets.only(bottom: 80),
+              width: 100,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.teal, width: 2.0)))),
+                child: Text('Sign Up'),
+                onPressed: () {},
+              ),
+            ),
           ),
-          ElevatedButton(
-            style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: BorderSide(color: Colors.teal, width: 2.0)))),
-            child: Text('Login'),
-            onPressed: () {},
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 80),
+              width: 100,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.teal, width: 2.0)))),
+                child: Text('Login'),
+                onPressed: () {},
+              ),
+            ),
           ),
         ],
       ),
-
     );
-
   }
 }
